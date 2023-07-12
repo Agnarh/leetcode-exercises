a491128f9b7b54b4e7ea9e2567c63493ff78d8b4
Iurii Perederii <uyuddar@gmail.com>
2023-07-13 00:09:16 +0400

    1 exercise

--- 1-two-sum.ts
+++ 1-two-sum.ts
@@ -1,7 +1,19 @@
 function twoSum(nums: number[], target: number): number[] {
-    return [];
+    const hash = nums.reduce((acc, x, i) => {
+        acc[x] = i;
+        return acc;
+    }, {} as Record<number, number>);
+
+    for (let index = 0; index < nums.length; index++) {
+        const curr = nums[index];
+        const other = hash[target - curr];
+
+        if (other && other !== index) {
+            return [index, other];
+        }
+    }
 }
 
 export const run = () => {
-    return twoSum([2, 7, 11, 15], 9);
+    return twoSum([1, 3, 4, 2], 6);
 };
