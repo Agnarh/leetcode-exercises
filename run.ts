00db47447f72611a4ff0e0995851d96b2c8c1f44
Iurii Perederii <uyuddar@gmail.com>
2023-07-13 10:52:10 +0400

    Update of running script

--- run.ts
+++ run.ts
@@ -1,5 +1,7 @@
 async function start() {
-    const { run } = await import(`./${process.argv[2]}.ts`);
+    const arg = process.argv[2];
+    const fileName = !!arg.match(/^\.\\.+\.ts$/gi) ? arg : `./${process.argv[2]}.ts`;
+    const { run } = await import(fileName);
 
     console.log(run());
 }
